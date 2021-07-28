0b8dbe09f8b062d08d71e51d4e13ef089c378387
Raeski <gustavoraeski@outlook.com>
2021-07-28 13:02:26 -0300

    Create class Video

--- src/main/java/com/challenge/alura/AluraApplication.java
+++ src/main/java/com/challenge/alura/AluraApplication.java
@@ -1,9 +1,11 @@
 package com.challenge.alura.Alura;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class AluraApplication {
 
 	public static void main(String[] args) {
