334929e65aca31250585b83c1d9692c6c8f392e2
Raeski <gustavoraeski@outlook.com>
2021-08-05 16:48:56 -0300

    Create class for tests Video Controller

---
+++ src/test/java/com/challenge/alura/controller/VideoControllerTest.java
@@ -0,0 +1,11 @@
+package com.challenge.alura.controller;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+class VideoControllerTest {
+
+}
